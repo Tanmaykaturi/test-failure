36ac6bfea9994dd5ee16ca01aacb9dace3d32c5d
Tanmaykaturi <159781384+Tanmaykaturi@users.noreply.github.com>
2025-10-23 17:18:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 GITHUB_TOKEN = "ghp_abcdefghijklmnopqrstuvwxyz1234567890AB" 
+API_KEY = "AKIAIOSFODNN7EXAMPLE"
+password = "mySuperSecret123"
+
 
 def connect_to_service():
     print("Connecting with AWS key:", AWS_ACCESS_KEY_ID)
